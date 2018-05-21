939fb54fed8544550405223e645305031c8137e4
Tom Schouten <tom@zwizwa.be>
2018-05-21 12:49:57 -0400

    cleanup

--- hs/Pru.hs
+++ hs/Pru.hs
@@ -3,33 +3,27 @@ module Pru where
 
 
 class Monad m => Pru m where
-  reg  :: String -> m Operand
-  asm2 :: (Operand -> Operand -> Instruction) -> Operand -> Operand -> m ()
+  reg :: String -> m Op
+  op2 :: (Op -> Op -> Ins) -> m Op -> m Op -> m ()
 
-data Operand = Reg String deriving Show
+data Op = Reg String deriving Show
+data Ins = Mov Op Op deriving Show
 
-data Instruction = Mov Operand Operand deriving Show
-
-r10 :: Pru m => m Operand
+r10 :: Pru m => m Op
+r11 :: Pru m => m Op
 r10 = reg "R10"
-r11 :: Pru m => m Operand
 r11 = reg "R11"
 
-mov :: Pru m => m Operand -> m Operand -> m ()
+mov :: Pru m => m Op -> m Op -> m ()
 mov = op2 Mov
 
 -- Debug: print to console.
 instance Pru IO where
   reg r = return $ Reg r
-  asm2 o a b = do
-    putStrLn $ show $ o a b
-    return ()
+  op2 ins a b = do
+    a' <- a ; b' <- b
+    putStrLn $ show $ ins a' b'
 
-op2 o a b = do
-  a' <- a;
-  b' <- b
-  asm2 o a' b'
-  
 
 trace = id
 
--- hs/test-pru.hs
+++ hs/test-pru.hs
@@ -2,7 +2,7 @@ import Pru
 
 main = do
   mov r10 r11
-
+  mov r11 r10
     
   
   
