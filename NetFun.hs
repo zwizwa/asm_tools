5769f4f1ed1e6397c732fb8fdebd0f014ccd356c
Tom Schouten <tom@zwizwa.be>
2018-06-27 16:21:27 -0400

    NetFun monad infrastructure

--- NetFun.hs
+++ NetFun.hs
@@ -1,11 +1,15 @@
--- Convert a network (graph) to a function.
+-- Given component semantics, convert a netlist to a function.
 
--- A network consists of:
--- . a set of n-port instances, each with a type
--- . a set of n-port types, implementing I/O functions (parameterized)
--- . a set of nets, mapping equating (instance,port)
+-- Use terminology used in circuit netlist:
+-- . a pin is identified by a component id and a port id
+-- . a net is a set of pins
+-- . a netlist is a set of nets
+-- . each component instance can be mapped to a behaviour/semantics
 
--- Sets are represented as lists.
+-- Note that this is different from "net" indicating network.  Also a
+-- "netlist" isn't really a list, it is a set.
+
+
 
 -- It is possible to convert a network to a function as long as
 -- n-ports are functions.
@@ -28,62 +32,65 @@ import Data.Map.Lazy (Map(..))
 import Data.Set (Set(..))
 import qualified Data.Map.Lazy as Map
 import qualified Data.Set as Set
+import qualified Data.Set.Extra as Set
 import Control.Monad.State
+import Control.Monad.Reader
 import Control.Applicative
+import Data.Foldable
+import Data.Either
 
--- A Fun is a map from input to output, together with a type
--- specification.
-newtype Fun = Fun (FunType, FunImpl)
-type FunImpl  = Map Port Value -> Map Port Value
-type FunType = (Set Port, Set Port)
-
-instance Show Fun where
-  show (Fun (t, i)) = show t
+-- A Fun is a map from input to output.
+-- If the output is empty, it is assumed the input is incomplete.
+type Fun v = Map Port v -> Map Port v
 
 
--- A Net is a set of Node.  A Node is a set of NPort Ports.  For each
--- NPort there is a type function.
-type Net = (NPort -> NPortType, Set Node)
-type Node = (NPort, Port)
+-- It seems that FunType is not needed.  The necessary information can
+-- be encoded in FunImpl: if not all inputs are available, then output
+-- map is empty.
+
+-- Basic collection hierarchy.
+type Netlist = Set Net
+type Net     = Set Pin
+type Pin     = (Component, Port)
+
+-- Identifiers are implemented as strings.  When appropriate, the
+-- context contains dictionaries that can resolve these to other
+-- types.
+type Port  = String
+type Component = String
 
 -- It is not necessary to define the component list seprately, as it
 -- can be derived directly from the graph:
-nports :: Net -> Set NPort
-nports (_, nodes) = Set.map fst nodes
+nports :: Netlist -> Set Component
+nports = Set.map fst . Set.flatten
 
--- A semantics _instance_ assigns each NPort (not NPortType!) to a
--- particular Fun.  In general we would like to have a global
--- configuration that can be decoded into a configuration for each
--- NPort to associate a Fun on a component by component basis.
-type Semantics = NPort -> Fun
+
+
+-- Meaning is created by mapping individual components to functions.
+-- Later we would like to parameterize this, when components can have
+-- multiple meanings, e.g. to implement I/O direction.
+type Semantics v = Component -> Fun v
 
 -- The evaluation of a network can only be done relative to semantics.
 -- But it is important to note that given a semantics, we can
 -- "flatten" everything into a single function through signal
 -- propagagion.  This is the main function we're interested in.
-eval :: Semantics -> Net -> Fun
+eval :: Semantics v -> Netlist -> Fun v
 eval = undefined
 
 -- In general this will need a way to perform proper "variable
--- renaming" for port identifiers.  This is why Port is implemented as
--- a list.
-rename :: (NPort, Port) -> Port
-rename (np,p) = np:p
+-- renaming" for port identifiers.  .
+rename :: (Component, Port) -> Port
+rename (np,p) = np ++ ":" ++ p
 
 
 -- Some temporary stubs for base types.  The String types are
 -- identifiers that can be used for value retrieval in certain
 -- contexts.
-type Port = [String]
-type NPort = String
-type NPortType = String
-
--- Only support two-valued logic.
-type Value = Bool
 
 -- Configuration can be added later.  The idea is to create a family
 -- of representations.
-type FunFamily = Config -> Fun
+type FunFamily v = Config -> Fun v
 type Config = ()   
 
 
@@ -95,25 +102,84 @@ type Config = ()
 -- constraints on how function semantics can be assigned to prots, so
 -- it is possible multiple drivers are present.
 
-type EvalState = ()
+type EvalState v = Map Int v
+type EvalEnv v = (Semantics v, Map Int Net)
+nets :: M v (Map Int Net)
+nets = do (_,nets') <- ask ; return nets'
 
-newtype M t = M { runM :: StateT EvalState (Either String) t } deriving
-  (Functor, Applicative, Monad, MonadState EvalState)
+newtype M v t = M { runM :: ReaderT (EvalEnv v) (StateT (EvalState v) (Either String)) t } deriving
+  (Functor, Applicative, Monad,
+   MonadReader (EvalEnv v),
+   MonadState (EvalState v))
 
--- Start with a simpler type first.
-eval' :: Semantics -> Net -> FunImpl
-eval' sem net ins = outs where
-  outs = Map.empty
+-- Inner routine.
+
+-- FIXME: type confusion about ins.  from the outside, we need to
+-- drive pins, so the interpretation should reflect that.  this kind
+-- of confusion is really typical, missing a layer of wrapping.  For
+-- now eval' takes pins as input.
+
+-- Basic algorithm:
+-- . for each input pin, find the corresponding net
+-- . if net already has a value, raise an error
+-- . set net value
+
+
+
+type Signals v = Map Pin v
+eval' :: Semantics v -> Netlist -> Signals v -> Either String (Signals v)
+eval' sem netlist i = o where
+  -- Assign each net a unique id.
+  nets = Map.fromList $ zip [0..] $ toList netlist
+  env = (sem, nets)
+  o = case runStateT (runReaderT (runM meval') env) Map.empty of
+        Left msg -> Left msg
+        Right ((), state) -> Right Map.empty
+
+meval' :: M v ()
+meval' = return ()
+
+-- A pin can only be in one net.
+net :: Pin -> M v (Int, Net)
+net pin = do
+  nets' <- nets
+  case Map.toList $ Map.filter (elem pin) nets' of
+    [kv] -> return $ kv
+    nets -> fail $ "net: pin in zero or multiple nets: " ++ show (pin,nets)
+
+
+
+
+
+
 
 printl :: Show s => [s] -> IO ()
 printl = sequence_ . (map print)
 
-test = print $ eval' sem net ins where
-  ins = Map.empty
-  sem _ = Fun (typ, impl)
-  typ = (typ_in, typ_out)
-  typ_in = Set.empty
-  typ_out = Set.empty
-  impl _ = Map.empty
-  net = (compType, Set.empty)
-  compType _ = "notype"
+
+
+test = print $ eval' sem netlist ins where
+
+  -- Input/output "connector" pins.
+  in_pin  = ("conn","in")
+  out_pin = ("conn","out")
+
+  -- We'll drive the input connector pin.
+  ins = Map.fromList [(in_pin, True)]
+
+  -- A single functional 2-port component, mapping the value of port
+  -- "1" to port "2", negating it.
+  inverter i =
+    case Map.lookup "1" i of
+      (Just v) ->  
+        Map.fromList [("2", not v)]
+      Nothing ->
+        Map.empty
+
+  -- Component u1 is assigned the inverter semantics.
+  sem "u1" = inverter
+    
+  netlist = Set.fromList [n1,n2]
+  n1 = Set.fromList [("u1","1"), in_pin]
+  n2 = Set.fromList [("u1","2"), out_pin]
+
