b614122df806c160ee3958e269690e657cd1fc96
Tom Schouten <tom@zwizwa.be>
2018-06-29 13:01:31 -0400

    SMap.hs

--- SMap.hs
+++ SMap.hs
@@ -3,6 +3,11 @@ import Data.Set(Set)
 import Prelude hiding (lookup)
 import qualified Data.Set as Set
 
+-- FIXME: This is inconsistent.  The problem is that a lookup can
+-- match multiple things.  How to deal with that?
+
+
+-- Interesting problem though, but there has to be a simpler way.
 
 -- In the context of netlists...
 
@@ -24,42 +29,68 @@ import qualified Data.Set as Set
 
 -- Invariant: all elements of the list are unequal in terms of the
 -- transitive closure of non-disjointness.  For each constructor below
--- it needs to be checked that this invariant holds.
+-- it needs to be checked that this invariant (I) holds.
 newtype SMap a b = SMap { unSMap :: [(Set a, Set b)] }
 
 
+-- I: trivial
 empty :: SMap a b
 empty = SMap []
 
-insert :: (Ord a, Ord b) => Set a -> Set b -> SMap a b -> SMap a b
-insert a b m@(SMap l) = case lookup' a m of
-  Nothing ->
-    SMap ((a,b):l)
-  Just (a',b') ->
-    SMap ((Set.union a a', Set.union b b') : (unSMap $ remove a' m))
+-- I: reduction, so I->I
+remove :: Ord a => Set a -> SMap a b -> SMap a b
+remove a (SMap l) = SMap $ filter (not . equal a . fst) l
 
-remove :: Set a -> SMap a b -> SMap a b
-remove = undefined
+-- I: doesn't hold.
+insert :: (Ord a, Ord b) => Set a -> Set b -> SMap a b -> SMap a b
+insert a b m@(SMap l) = m' where
+  (a', b') = lookup' a m
+  m' = case Set.null a' of
+    True -> 
+      SMap ((a,b):l)
+    False ->
+      SMap ((Set.union a a', Set.union b b')
+            : (unSMap $ remove a' m))
+
 
 -- Non-constructors
 
-lookup a m = fmap snd $ lookup' a m
-         
-lookup' :: Ord a => Set a -> SMap a b -> Maybe (Set a, Set b)
-lookup' a (SMap l) = f l where
-  f [] = Nothing
-  f ((a',b):l) = case ntEqual a a' of
-    True -> Just (a',b)
-    False -> f l
+-- lookup a m = fmap snd $ lookup' a m
+
+
+-- Beware of the analogue of "lookup".  (FIXME: rename it as "collect" ?)
+--
+-- Given the map
+--   {1,2} => {a}
+--   (3,4} => {b}
+--
+-- What is lookup {1,3} ?  Based on the local definition of equalitly,
+-- to be {a,b}, so it is clear that lookup performs some collapse
+-- based on the key.
+--
+-- In that context it makes sense to return both the new name and value.
+-- It's probably a good idea to also implement partitioning along the way.
+
+lookup' :: (Ord a, Ord b) => Set a -> SMap a b -> (Set a, Set b)
+lookup' a (SMap l) = kv where
+  kv = foldr merge (Set.empty, Set.empty) l
+  merge (a, b) s@(a', b') = case equal a a' of
+    True  -> (Set.union a a', Set.union b b')
+    False -> s
+
+
+
+
 
 -- Non-primitives
 
-element :: (Ord a, Eq b) => Set a -> SMap a b -> Bool
-element e m = Nothing /= lookup e m
+element :: (Ord a, Ord b) => Set a -> SMap a b -> Bool
+element e m = not $ Set.null ks where
+  (ks,_) = lookup' e m
 
 
 -- Tools
 -- non-transitive equal
-ntEqual a a' = not $ Set.null $ Set.intersection a a'
+equal a a' = not $ Set.null $ Set.intersection a a'
 
 
