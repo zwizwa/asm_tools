1467156c29d5f2a4e161c74ad9671f4cb3f464c0
Tom Schouten <tom@zwizwa.be>
2018-06-29 14:25:51 -0400

    SMap.hs

--- SMap.hs
+++ SMap.hs
@@ -1,96 +1,104 @@
+-- Finite functions from sets to sets.
+
+-- TODO: This seems to make sense, so there is likely an existing
+-- implementation.
+
 module SMap where
 import Data.Set(Set)
 import Prelude hiding (lookup)
 import qualified Data.Set as Set
 
--- FIXME: This is inconsistent.  The problem is that a lookup can
--- match multiple things.  How to deal with that?
-
-
--- Interesting problem though, but there has to be a simpler way.
-
 -- In the context of netlists...
+-- See papers/haskell.txt for more information
 
 -- To implement shorting properly, it makes sense to introduce the
--- idea of aliases, e.g. (Set NetName).  But to make this work in the
--- standard way it seems Eq and Ord are needed.  Eq based on
--- non-disjunction already violates transitivity.  See
--- papers/haskell.txt
+-- idea of aliases, e.g. instead of indexing nets based on a key
+-- NetName, index them as (Set NetName).
+
+-- This suggests the avenue of Map (Set a) b with two keys being
+-- inqual if the sets are disjoint.  However, that operation is not
+-- transitive.  Map needs Eq and Ord, so it doesn't seem like that
+-- will work out.
 
 -- It does seem possible to define a weaker notion of equality
 -- locally, inside the implementation of a Map-like abstraction.
 
--- Anyways, the details go deeper than expected.  So stick to the
--- basics.  We need a set-indexed map, mapping sets to sets.
-
--- TODO: Either this is meaningless, or there is already an
--- abstraction like this.
+-- Following through with an attempt to implement this leads to an
+-- invariant (keys in the map should be disjoint), and a central
+-- nontrivial "partition" operation.
 
 
--- Invariant: all elements of the list are unequal in terms of the
--- transitive closure of non-disjointness.  For each constructor below
--- it needs to be checked that this invariant (I) holds.
+
+-- Invariant: all elements of the list are unequal in the sense that
+-- their key sets are disjoint.  For each constructor below it needs
+-- to be checked that this invariant (I) holds.
 newtype SMap a b = SMap { unSMap :: [(Set a, Set b)] }
 
 
+-- The non-transitive generalized equality operator.
+
+-- ( TODO: is there a way to represent this so it can be transitively
+-- closed?  One idea is to turn it around, and actually use this
+-- implementation mapping to () as a representation. )
+
+overlap a a' = not $ Set.null $ Set.intersection a a'
+
+
+
 -- I: trivial
-empty :: SMap a b
+empty :: SMap k v
 empty = SMap []
 
 -- I: reduction, so I->I
-remove :: Ord a => Set a -> SMap a b -> SMap a b
-remove a (SMap l) = SMap $ filter (not . equal a . fst) l
+remove :: Ord k => Set k -> SMap k v -> SMap k v
+remove k (SMap kvs) = SMap $ filter (not . overlap k . fst) kvs
 
--- I: doesn't hold.
-insert :: (Ord a, Ord b) => Set a -> Set b -> SMap a b -> SMap a b
-insert a b m@(SMap l) = m' where
-  (a', b') = lookup' a m
-  m' = case Set.null a' of
-    True -> 
-      SMap ((a,b):l)
-    False ->
-      SMap ((Set.union a a', Set.union b b')
-            : (unSMap $ remove a' m))
+-- I: induction: if I holds for the input sets, it holds because we
+-- are joining a matching singleton with a disjoint partition.
+insert :: (Ord k, Ord v) => Set k -> Set v -> SMap k v -> SMap k v
+insert k v m@(SMap l) = m' where
+  ((k', v'), SMap disjoint) = partition k m
+  m' = SMap $ case Set.null k' of
+    True  -> ((k,v):l)
+    False -> ((Set.union k k', Set.union v v') : disjoint)
 
 
--- Non-constructors
 
--- lookup a m = fmap snd $ lookup' a m
-
-
--- Beware of the analogue of "lookup".  (FIXME: rename it as "collect" ?)
+-- The key routine is "partition", which is a generalization of
+-- "lookup", returning not only the requested value, but also the
+-- maximum of the key, and the maximal disjoint submap.
 --
--- Given the map
+-- Motivation: given the map
 --   {1,2} => {a}
 --   (3,4} => {b}
 --
 -- What is lookup {1,3} ?  Based on the local definition of equalitly,
--- to be {a,b}, so it is clear that lookup performs some collapse
--- based on the key.
+-- there seems to be no other sensible answer than {a,b}.  However in
+-- that context, the key we used to perform the lookup can be
+-- maximized to {1,2,3,4}.
 --
--- In that context it makes sense to return both the new name and value.
--- It's probably a good idea to also implement partitioning along the way.
-
-lookup' :: (Ord a, Ord b) => Set a -> SMap a b -> (Set a, Set b)
-lookup' a (SMap l) = kv where
-  kv = foldr merge (Set.empty, Set.empty) l
-  merge (a, b) s@(a', b') = case equal a a' of
-    True  -> (Set.union a a', Set.union b b')
-    False -> s
+-- So it makes sense to return both the value and the maximized key.  
+--
+-- In the context of other operations, it seems also natural to
+-- collect and return the disjoint part.  This is useful for
+-- implementing insert.
 
 
+-- I: induction: only partitions are performed to construct the output
+-- map, so I holds if it it holds for the input.
+
+partition :: (Ord a, Ord b) => Set a -> SMap a b -> ((Set a, Set b), SMap a b)
+partition a (SMap l) = p where
+  p = foldr merge ((Set.empty, Set.empty), SMap []) l
+  merge p@(a, b) (c@(a', b'), ps'@(SMap ps)) = case Set.null a of
+    True  -> (c, SMap (p:ps))
+    False -> ((Set.union a a', Set.union b b'), ps')
 
 
-
--- Non-primitives
-
+-- Other Non-primitives
 element :: (Ord a, Ord b) => Set a -> SMap a b -> Bool
 element e m = not $ Set.null ks where
-  (ks,_) = lookup' e m
+  ((ks, _), _) = partition e m
 
-
--- Tools
--- non-transitive equal
-equal a a' = not $ Set.null $ Set.intersection a a'
 
 
