e02838adc7a0761f4cc37140970c71dafcfe7bd7
Tom Schouten <tom@zwizwa.be>
2018-07-24 19:43:17 -0400

    cleanup

--- Causal.hs
+++ Causal.hs
@@ -13,6 +13,8 @@ import Seq
 import SeqTerm
 import SeqEmu
 
+-- Most generic: use standard Haskell type classes.
+
 class Applicative f => Causal f where
   close :: (a -> (a, b)) -> a -> f b
 
@@ -21,9 +23,17 @@ instance Causal [] where
     f s = (o : f s) where
       (s, o) = u s
 
-integral :: (Num t, Causal f) => t -> f t
-integral = close (\s -> (s + 1, s))
+-- This is straightforward
+counter :: (Num t, Causal f) => t -> f t
+counter = close (\s -> (s + 1, s))
 
+-- But this needs a different approach.
+-- integral :: (Num t, Causal f) => t -> f t -> f t
+-- integral init ins =
+  
+
+
+-- Allow Specialized instances.
 
 data family Stream a
 data instance Stream Int = StreamList [Int]
@@ -38,4 +48,24 @@ data instance Stream (SeqEmu.R  S) = StreamEmu  (SeqEmu.M  (SeqEmu.R  S))
 
 -- Options: add these as associated data types.
 -- Or make it much more general and just specialize for the SeqTerm and SeqEmu variants.
+
+-- Goal: reify integral
+
+-- integral' :: Seq m r => m (r S) -> m (r S)
+-- integral' = integral
+
+
+-- instance Functor Stream where
+  
+
+
+
+
+-- Now what is the point of this?  To separate two things:
+-- a) The scalar types for combinatorial operations
+-- b) The sequential feedback
+
+-- Now, does this need to be an associated type or not?  And
+-- associated to what class exactly?
+
 
