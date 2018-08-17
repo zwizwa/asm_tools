42ff3ee04ae0557904e02b72ee36579ef9b05156
Tom Schouten <tom@zwizwa.be>
2018-08-17 00:15:22 -0400

    cleanup

--- CPU.hs
+++ CPU.hs
@@ -71,71 +71,28 @@ import Data.Binary.Get
 
 
 
--- Original notes on stack macchines:
-
--- Memory seems to be the most important component.  I'm going to
--- target the iCE40, which has a bunch of individual memories,
--- allowing separate busses for instruction, data and return stack,
--- and the rest bundled as data memory.
-
--- At every clock, each of the 4 memories has a word sitting on its
--- read port:
--- i: current instruction
--- d: 2nd on stack  (top is in a register)
--- r: return address (ip is the instruction memory's write port)
-
--- The instruction word drives the decoder, which drives all the
--- muxes.
-
--- It seems that reading out instructions is the most useful thing to
--- start with.  This could be used for specialized sequencers that are
--- not necessarily general purpose CPUs.  This can then be gradually
--- extended to more abstract operations.
-
-
--- The main problem for building a CPU is to properly decompose the
--- decoder.  I'm not sure how to do this exactly, so just start in an
--- ad-hoc way.
-
--- There is some arbitraryness here: a hierarchy is created in the
--- nesting of the "close" operations.  The guideline is to abstract
--- away a register as soon as possible, i.e. move it to the inner part
--- of the hierarchy.
-
--- At the very top, there is:
--- . instruction memory access:
---   . read:  program sequencing
---   . write: bootloader
--- . BUS I/O (i.e. containing GPIO)
-
--- Each hierarchy level is an adaptation.  closeIW will abstract the
--- inner decoder as an iw -> jump operation, and insert the necessary
--- logic to either just advance to the next instruction, or perform a
--- jump.
-
+-- The sequencer and the instruction memory can be decoupled using
+-- these interfaces:
 data Ins r  = Ins {
   insWord :: r S,
   insPtr  :: r S
   }
-
 data Control r = Control {
   controlLoop :: r S,
   controlJmp  :: r S,
   controlArg  :: r S
   }
 
--- The interface to the outside consists of GPIO and iMem write access.
--- Maybe it is time to start parameterizing.
-
+-- The CPU can't write instruction memory.  The write port is
+-- connected to the external world through SPI.
 data IMemWrite r = IMemWrite {
   iMemWriteEn   :: r S,
   iMemWriteAddr :: r S,
   iMemWriteData :: r S
   }
 
--- Reads are closed, so sizes are necessarily taken from the size of
--- the write bus address and data registers.  If there is none
--- (e.g. if IMem is a ROM), then this can be used to specify size.
+-- That interface also determines sizes.  If there is no writer
+-- (e.g. a ROM), the following stub can be used to specify size.
 noIMemWrite :: Seq m r => Int -> Int -> IMemWrite r
 noIMemWrite ibits abits = IMemWrite e w d where
   e = cbit 0
@@ -143,20 +100,22 @@ noIMemWrite ibits abits = IMemWrite e w d where
   d = cbits ibits 0
 
 
+-- The instruction sequencer fetches instructions from the instruction
+-- memory and passes them to a custom 'execute' module, expecting back
+-- a control instruction to describe what to do next.
 sequencer :: Seq m r =>
   IMemWrite r
   -> r S
   -> (Ins r -> m (Control r, o))
   -> m o
-
 sequencer (IMemWrite wEn wAddr wData) run execute = do
   t_wAddr <- stype wAddr
   t_wData <- stype wData
   closeMem [t_wData] $ \[iw] -> do
     (ipNext, o) <- closeReg [t_wAddr] $ \[ip] -> do
-      -- Execute instruction, which produces control flow information.
       iw'    <- if' run iw 0
       ipCont <- inc ip
+      -- Execute instruction, which produces control flow information.
       (Control loop jump ipJump, o) <- execute (Ins iw' ipCont)
       rst      <- inv run
       [ipNext] <- cond
@@ -170,63 +129,6 @@ sequencer (IMemWrite wEn wAddr wData) run execute = do
       return ([ipNext], (ipNext, o))  -- comb ipNext to avoid extra delay
     return ([(wEn, wAddr, wData, ipNext)], o)
 
--- A simple test for sequencer:
--- . program outputs iw as output
--- . tied to a memory writer defined in the test lib
-
--- The next thing should be to test this on hardware, but this
--- requires code initialization primitives.
-
-
--- The origianl problem that drove this exploration is meanwhile
--- implemented on PRU.  These were the instructions needed:
--- 
--- a) loop n times
--- b) write UART byte, wait until done
--- c) wait
--- d) set I/O
--- e) read I/O into memory and advance pointer
-
--- To implement loops, it would be useful to have a stack to be able
--- to have nested loop counters.  This would mean less registers.  I'm
--- not going to be able to make this simpler than making a small forth
--- machine..  This way:
-
--- UART out can be bit-banged.
--- Multiple counters not needed for timing control.
--- No "wait" instruction needed: instruction counting suffices.
--- Add a data stack when needed.  Probably a single top register is enough.
-
--- The basic instructions seem straightforward.  This is just a
--- decoder that fans out into mux controls.  The unknown part to me is
--- the call/return.
-
--- Call:   move IP+1 -> rtop write port
---         inc rpointer
---         set ip from instruction word
--- Ret:    dec rpointer
---         move rtop -> IP
-
--- This could also be microcoded:
--- a) load literal into rdata
--- b) increment rstack
--- c) unconditional jump
-
--- The operations that can be reused are:
--- write, postinc  (stacks + buffers)
--- read, predec
-
--- So there is a clear tradeoff between the complexity of the
--- instruction decoder, and the amount of instructions needed.
-
--- Where to start?  Conditional memory write.
-
--- So for unidirectional flow, this is easy.  For bi-directional such
--- as a stack, two pointers need to be maintained.  It might be
--- simplest to initialize them such that the write/read operation can
--- happen immediately?  Both will have individual adders.  Maybe not a
--- good idea?
-
 
 
 -- The important realization is that what a CPU does is to manipulate
@@ -236,7 +138,7 @@ sequencer (IMemWrite wEn wAddr wData) run execute = do
 -- I'm adopting a slight modification of the memory bus already used.
 -- The addition is a busReadRdy signal, which allows reads to take
 -- more than one cycle, and allows the cpu to wait for a particular
--- signal to appear.  Seperate read/write addresses are removed as the
+-- signal to appear.  Separate read/write addresses are removed as the
 -- CPU is kept simple: no read/write at the same time.
 
 data BusRd r = BusRd {
@@ -251,34 +153,13 @@ data BusWr r = BusWr {
 
 
 
--- Perform an operation and wait for it to finish.
--- Let's keep the operation abstract, so what this does is:
---
--- . first time the instruction is executed, the sub-machine is
---   enabled.  the sequencer will wait until the machine provides a
---   "done" flag, which will advance the instruction pointer.
---
--- . it seems simpler to split this into "start" and "wait"
---   instructions.
---
-
-
--- Some testing
-
-
-
-
-data OpCode = IJMP
-
-opcode :: OpCode -> Int
-opcode IJMP = 0x80
-
-
-
+-- Instructions
 
 -- This needs to grow, but it seems simplest to keep the encoding
--- abstract, so it can be optimized later on.  Do not rely on the
--- numeric values of these.
+-- abstract, with both the assembler and the instruction decoder using
+-- the functions below.  The only reason not to change the numbers is
+-- that a certain familiarity arises after some debugging.
+
 o_nb_bits  = 4
 
 o_nop   = 0
@@ -292,28 +173,17 @@ o_loop  = 7
 o_call  = 8
 o_ret   = 9
 
+
 -- Use Forth for flow control structures.
 
--- Note that this is a Forth /
--- Asm hybrid: operations do not have to be nullary.
-
-for' n m = do push n ; for ; m ; next
-for   = Forth.mark
-next  = Forth.cpop >>= loop
-begin = Forth.mark
-again = Forth.cpop >>= jmp
-
--- Note that this is a Forth / Asm hybrid: operations do not have to
--- be nullary.
-
+-- Note that this is a Forth / Asm hybrid embedded in a Monad:
+-- primitive operations do not have to be nullary.
 ins i = Forth.save [i]
 
 i1 :: Int -> Int -> Forth.M ()
 i1 opc arg = ins $ opc `shiftL` (16 - o_nb_bits) .|. (arg .&. 0xFF)
-
 i0 c = i1 c 0
 
--- These will be used directly to construct programs, so use simple names.
 nop   = i0 o_nop
 jmp   = i1 o_jmp
 push  = i1 o_push
@@ -321,11 +191,27 @@ read  = i1 o_read
 write = i1 o_write
 swap  = i0 o_swap
 loop  = i1 o_loop
-drop  = write 0xff
+drop  = write (-1)
 call  = i1 o_call
 ret   = i0 o_ret
 
+-- In any case, a monad implementing a Forth compile stack is
+-- convenient if only because of familiarity.
+for   = Forth.mark
+next  = Forth.cpop >>= loop
+begin = Forth.mark
+again = Forth.cpop >>= jmp
+
+
+-- But with Haskell as meta-langauge, there is not a lot of need for
+-- specific control structures beyond what is offered by the primitive
+-- instructions.  E.g.:
+for' n code = do push n ; for ; code ; next
 forever m = begin >> m >> again
+
+
+
+-- For sending to a running device, pack programs as binary.
 
 -- Put it in a representation that can be sent as bytes over SPI.
 writeProgram :: String -> Forth.Program -> IO ()
@@ -352,26 +238,8 @@ unpackProgram bytes = prog where
 
 
 
+-- Decode, Execute
 
-
--- Each instruction can have push/pop/write/nop wrt imm?  It seems
--- possible that stack can be manipulated in parallel with bus
--- transfer.
-
--- But let's not make this too complicated.  Some observations:
-
--- . This is for very low level, specialized code.  It will never
---   necessary to manipulate addresses as data, so address for read,
---   write can always come from the immediate word.  The data itself
---   might be manipulated.
-
-
--- It's probably ok to instantiate it fully even if certain
--- instructions are not used.  Yosys/abc removes unused logic.
-
--- Still, this can use some decomposition.  For now, because there are
--- not many instructions, use one-hot encoding to keep the logic
--- simple.
 
 -- The "read" and "write" directions are from the perspective of the
 -- CPU (i.e. the programmer).
@@ -479,12 +347,12 @@ stack_machine  nb_stack (BusRd bus_rdy bus_data) (Ins iw ip) = do
 -- Close over bus master (cpu) and bus slaves (peripherals) to create
 -- a system on chip.
 
--- FIXME: make register addresses abstract.
+-- Register addresses are abstract similar to instruction encodings.
 
 uart_rx = 0 :: Int
 uart_tx = 1 :: Int
 dbg     = 2 :: Int
--- 0xFF is the void for "drop"
+-- -1 is the void used for "drop"
 
 bus [rx] (BusWr wEn addr wData) = do
   
@@ -518,22 +386,20 @@ bus [rx] (BusWr wEn addr wData) = do
     (return [0,0])
   return (BusRd rStrobe rData, [tx, dbg'])
 
--- For now these are fixed to 16 bit instruction words and 8 bit address.
+-- For now these are fixed to 16 bit instruction words
 spi_boot nb_bits cs sck sda = do
   (wc, w) <- sync_receive Mode3 16 cs sck sda
   a <- arrPtr (bits nb_bits) cs wc
-  -- a <- fifoPtr (bits 8) wc
   return $ IMemWrite wc a w
 
-rom_boot :: Seq m r => IMemWrite r
-rom_boot = noIMemWrite 16 8  
+-- rom_boot :: Seq m r => IMemWrite r
+-- rom_boot = noIMemWrite 16 8
 
 soc :: Seq m r => [r S] -> m [r S]
 soc [rx,cs,sck,sda] = do
 
-  -- A soc is the bundling of a CPU, an instruction memory (sequencer
-  -- and boot), a peripheral bus, and a reset circuit.
-
+  -- A soc is the bundling of a CPU, an instruction memory, sequencer,
+  -- boot circuit, a peripheral bus, and a reset circuit.
   let
     -- Instruction width is fixed at 16 bits for now
     ins_bits  = 16
@@ -543,17 +409,17 @@ soc [rx,cs,sck,sda] = do
   -- Do not update the instruction pointer at the first instruction,
   -- as rData will be invalid.  The first cycle is used to perform the
   -- first instruction read.  After that, instruction pointer is updated.
-  reset <- seq01 -- 0,1,1,1....
+  mem_wait <- seq01 -- 0,1,1,1....
 
   -- Processor can be restarted with a program from SPI.
-  reset <- reset .& cs
-  boot  <- spi_boot imem_bits cs sck sda
+  nrst <- mem_wait .& cs
+  boot <- spi_boot imem_bits cs sck sda
   
   let 
     -- Couple CPU and instruction memory
     bus_master busIn = busOut where
       execute = stack_machine 4 busIn
-      busOut = sequencer boot reset execute
+      busOut = sequencer boot nrst execute
   
   -- Couple bus master and slave through bus registers.
   closeReg [bit, bits imem_bits] $ \[rStrobe,rData] -> do
