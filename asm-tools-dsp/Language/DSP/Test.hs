4cfd30bb7ddfde2576a412624562c982c34f59c8
Tom Schouten <tom@zwizwa.be>
2023-07-12 13:02:58 +0200

    Language.DSP.Test looking for a spec

---
+++ asm-tools-dsp/Language/DSP/Test.hs
@@ -0,0 +1,159 @@
+module Language.DSP.Test where
+
+-- See also older drafts at the bottom.  Trying to figure out the
+-- simplest version of the problem.
+
+-- A simpler mapping:
+--
+-- . The user of the DSL "instantiates combinators".  The combinators
+--   are defined as manipulation of C syntax.  This should avoid
+--   monads in the DSL itself.
+--
+-- . For now assume that 1) every _combinator result_ has a C language
+--   implementation (e.g. no blocking state machine code), and 2) C
+--   structs can be used to bundle variables, assuming the compiler
+--   will optimize away the intermediate structs.  If those
+--   assumptions fail it would still be ok to revisit later.  Note
+--   that the combinator itself might not be representable in C
+--   syntax, only as a map from C syntax to C syntax.
+--
+-- . C syntax can be replaced by an embedding of a subset of C/LLVM
+--   inside Haskell.  It seems simpler to implement the heavy lifting
+--   (the syntax combination) inside Haskell.
+--
+-- . Use integer-indexed types at the Haskell level.  Full dependent
+--   types are probably not necessary, but I do need to be able to
+--   specialize code to a fixed vector size.
+
+
+-- Some key points
+-- . The DSL at the Haskell end is combinator only
+-- . Primitive combinators in C (or C wrapper in Haskell)
+-- . No monads in the DSL: it maps signals to signals
+
+
+-- Note that a lot of magic is encoded in the combinators, e.g. think
+-- of the causal feedback state introduction which is far from
+-- trivial.
+
+-- Example: apply the pure function (a,b)->c to the signal rate signal
+-- S a and the control rate signal C b and yield a signal rate signal
+-- S c.
+--
+-- :: (a -> b -> c) -> (S a -> C b -> S c)
+--
+-- Here 'S' and 'C' are fairly abstract.  The way these containers are
+-- implemented can be quite involved.
+
+
+-- Next: write down the full type, then try to create an example (a
+-- proof).  Because the types are so abstract, all the detail is going
+-- to be in the implementation of the combinator _and_ the
+-- representation of things like S a, C b.
+--
+-- From previous experiments: the signals are a pair of initial value
+-- and generator function.  Thinking about making all initial values
+-- zero, but for some finite iterative processes that might not be a
+-- good idea.
+--
+-- Both C a and S a have the same shape: initial value and generator:
+-- (s,s->(s,o)) but they are different in that they have a different
+-- sample rate, which means their different semantics is only
+-- observable in the implementation of a combinator.
+
+-- So let's start by giving everything a name.  The concrete signal
+-- with state type exposed.  This is the inside view, i.e. the
+-- implemenation.
+data SSO s a = SSO s (s -> (s, a))
+constSSO c = SSO () (\() -> ((), c))
+
+-- Primitive data types are provided by the compilation target, which
+-- is C or some emulation in Haskell.  Keeping this simple for now as
+-- there is really no magic here.
+class PrimData a where
+  add :: a -> a -> a
+  mul :: a -> a -> a
+
+-- The first combinator the simplest one I can think of: scale a
+-- signal rate signal with a value.  It will present the first
+-- problem: how to compose hidden signals?
+
+-- Let's first do a concrete version, then try to abstract the
+-- composition mechanism in a type class.
+scaleSSO' :: PrimData a => SSO s a -> a -> SSO s a
+scaleSSO' (SSO s0 u) a = SSO s0 u' where
+  u' s = (s', a `mul` o) where (s',o) = u s
+
+-- My gut feeling says not to overengineer it and to tuck away _all_
+-- combinators in a type class, so define the existential wrappers
+-- like this.
+class PrimData a => SSOImpl s a where
+  scaleSSO :: (SSO s a) -> a -> (SSO s a)
+
+instance PrimData a => SSOImpl s a where
+  scaleSSO (SSO s0 u) a = SSO s0 u' where
+    u' s = (s', a `mul` o) where (s',o) = u s
+  
+
+data S a = forall s. (PrimData a, SSOImpl s a) => S (SSO s a)
+data C a = forall s. (PrimData a, SSOImpl s a) => C (SSO s a)
+
+scaleS :: S a -> a -> S a
+scaleS (S sso) a = S $ scaleSSO sso a
+
+scaleC :: C a -> a -> C a
+scaleC (C sso) a = C $ scaleSSO sso a
+
+
+
+test = do
+  putStrLn "Language.DSP.Test"
+
+
+
+
+
+
+
+
+-- Draft 1
+
+-- Compile HOAS to C.  This is modeled after Lure seq.lua except that
+-- all DSL type business is handled in the Haskell type system.
+
+-- Compilation works by "passing a semantics to HOAS".  In Haskell
+-- this is implemented as a type class parameterized by a
+-- representation monad.
+
+-- First: what is the compilation target?  Nested loops.  The purpose
+-- of the DSL is mostly in combinators.  The basic operators are all
+-- <lvalue> = <op> <rvalue> ...
+
+-- I know I spent a lot of time trying to find a good representation
+-- of "continuation".  A representation of a continuation could be:
+-- the (indexed) variable where the result is stored + next
+-- instruction to execute.  The control flow (next instruction) can be
+-- implicit for now.  However it seems important to make the base
+-- representation into CPS such that control structures can be added
+-- later.
+
+-- So the basic form is "dataflow as reduced CPS" or something.
+
+-- A toy example would be adding two vectors, which is an operation
+-- that takes a scalar CPS function and transforms it into a vector
+-- CPS function.
+
+-- To simplify, I am going to assume that the "code output" can be
+-- written linearly.
+
+-- Focusing on combinators, I need things like: convert async event
+-- stream + audio rate stream into audio rate stream, interpolating
+-- async events.
+
+-- Previous attempts have always put the CPS into the monad.
+
+-- Maybe this is all too lowlevel.  Focus on the combinators, the
+-- algebra.  What the DSL compiler needs to do is to decide to inline
+-- a combinator, or to implement it using a run-time indirection.
+-- That is all really.
+
---
+++ asm-tools-dsp/README.md
@@ -0,0 +1,3 @@
+This is not an independent project yet, i.e. it does not have a .cabal
+file.  See /i/exo/ghci-23.05/ghci_innerl.sh
+
